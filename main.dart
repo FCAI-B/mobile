5814fc47b225401c22b9614ed975f7515c16a374
B-FCAI <92329466+B-FCAI@users.noreply.github.com>
2022-05-07 15:17:01 +0200

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My Bar'),
+        ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('babies').snapshots(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) {
+              return const Text('Waiting');
+            } else {
+              return ListView(
+                shrinkWrap: true,
+                children: snapshot.data.docs.map<Widget>((DocumentSnapshot doc) {
+                  return ListTile(
+                    title: Text(doc.get('name')),
+                    subtitle: Text(doc.get('votes').toString()),
+                    onTap: () {
+                      //TODO update the document by increasing votes
+                    },
+                  );
+                }).toList(),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
